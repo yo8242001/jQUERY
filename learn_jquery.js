35daf1e264db94a4e4599fcc1a8fcfb4d8674923
yo8242001 <beyoung456@gmail.com>
2019-07-27 15:30:30 +0800

    新增盒子元素並添加類別

--- learn_jquery.js
+++ learn_jquery.js
@@ -5,9 +5,14 @@
 // function () {} 匿名函式
 
 $(document).ready(function () {
+    
     alert("網頁文件準備完成了~");
-$("h1").hide();
-$("h2").hide();
+
+    $("h1").hide();                 // 選取 h1 並隱藏
+    $("h2").hide();                 // 選取 h2 並隱藏
+
+    $("#box1").addClass("gold");    // 選取 #box1 並添加類別 gold
+
 });
 
 // 函式、方法
