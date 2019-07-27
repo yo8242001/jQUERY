b8ec7274eae2fdf7b62f26122f5f6af9d4b9ccfd
yo8242001 <beyoung456@gmail.com>
2019-07-27 17:05:40 +0800

    新增捲到到頁面上方效果

--- backtotop.js
+++ backtotop.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
     // #btnTop 按鈕點擊事件
     $("#btnTop").click(function (e) { 
         
-        alert("點了按鈕~");
+        // alert("點了按鈕~");
+
+        // 尋找 網頁與身體 動畫效果 ({捲動上方：0}，時間);
+        $("html, body").animate({scrollTop: 0}, 1000);
     });
 });
