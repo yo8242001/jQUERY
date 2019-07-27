003b6e8a36eae60b3ee8115f8d318629aaf22a74
yo8242001 <beyoung456@gmail.com>
2019-07-27 14:46:18 +0800

    添加網頁準備完成語法

--- learn_jquery.js
+++ learn_jquery.js
@@ -1,1 +1,16 @@
-alert("嗨~");
+// $() 搜尋物件
+// document 網頁文件
+// . 執行某件事
+// ready() 準備完成
+// function () {} 匿名函式
+
+$(document).ready(function () {
+    
+    alert("網頁文件準備完成了~");
+
+});
+
+// 函式、方法
+function A () {
+
+}
