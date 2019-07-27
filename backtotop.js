c87e0f0c348b2bdd573ead7561bbdb16424ae04b
yo8242001 <beyoung456@gmail.com>
2019-07-27 19:59:16 +0800

    自訂 API 與使用

--- backtotop.js
+++ backtotop.js
@@ -2,36 +2,53 @@
 $(document).ready(function () {
 
     // #btnTop 按鈕點擊事件
-    $("#btnTop").click(function (e) { 
-        
+    $("#btnTop").click(function (e) {
+
         // alert("點了按鈕~");
 
         // 尋找 網頁與身體 動畫效果 ({捲動上方：0}，時間);
-        $("html, body").animate({scrollTop: 0}, 1000);
+        $("html, body").animate({ scrollTop: 0 }, 1000);
 
     });
 
     // 函式會有小括號
 
-    $("#a1").click(function (e) { 
+    // $("#a1").click(function (e) { 
+    //     var top = $("#section1").position().top;
+    //     $("html, body").animate({scrollTop: top}, 500);
+    // });
 
-        var top = $("#section1").position().top;
-        $("html, body").animate({scrollTop: top}, 500);
-    });
 
-    $("#a2").click(function (e) { 
 
-        var top = $("#section2").position().top;
-        $("html, body").animate({scrollTop: top}, 500);
-    });
-    
+    // API 
+    // 自訂函式 (參數) { 內容 }
+    // btn - 要點選的按鈕選取器名稱
+    // pos - 要前往的區塊選取器名稱
+    // dur - 前往的時間
+    function scrollToPosition(btn, pos, dur) {
+
+        $(btn).click(function (e) {
+            var top = $(pos).position().top;
+            $("html, body").animate({ scrollTop: top }, dur);
+        });
+
+    }
+
+    // 使用 API
+    scrollToPosition("#a1", "#section1", 500);
+    scrollToPosition("#a2", "#section2", 1000);
+    scrollToPosition("#a3", "#section3", 1000);
+    scrollToPosition("#a4", "#section4", 1000);
+    scrollToPosition("#a5", "#section5", 1000);
+
+
     // alert($(window).scrollTop());
-    
-    $("#btnTop").fadeOut(0);                // 按鈕隱藏 - 也可設定在 CSS 內
+
+    // $("#btnTop").fadeOut(0);                // 按鈕隱藏 - 也可設定在 CSS 內
 
     // 元素 捲動 (匿名函式() {  });
-    $(window).scroll(function() {
-        
+    $(window).scroll(function () {
+
         if ($(window).scrollTop() > 200) {  // 如果 螢幕頂端 > 200
 
             $("#btnTop").fadeIn(500);       // 顯示按鈕
